37e0df836fcf93365c6660b45f1a023ea2185ac0
png261 <nhphuong.code@gmail.com>
2023-01-17 16:21:49 +0700

    Chapter2.2

--- src/Game.cpp
+++ src/Game.cpp
@@ -20,19 +20,19 @@ bool Game::init(const char *title, int xpos, int ypos, int width, int height, bo
         return false;
     }
 
-    SDL_Surface* pTempSurface = SDL_LoadBMP("assets/rider.bmp");
+    SDL_Surface* mTempSureFace = SDL_LoadBMP("assets/animate.bmp");
+    m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, mTempSureFace);
+    SDL_FreeSurface(mTempSureFace);
 
-    m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
-    SDL_FreeSurface(pTempSurface);
-
-    SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
-
+    m_sourceRectangle.w = 128;
+    m_sourceRectangle.h = 82;
     m_sourceRectangle.x = 0;
     m_sourceRectangle.y = 0;
-    m_destinationRectangle.x = 0;
-    m_destinationRectangle.y = 0;
+
     m_destinationRectangle.w = m_sourceRectangle.w;
     m_destinationRectangle.h = m_sourceRectangle.h;
+    m_destinationRectangle.x = 0;
+    m_destinationRectangle.y = 0;
 
     m_bRunning = true;
 
@@ -51,7 +51,7 @@ void Game::handleEvents() {
 } 
 
 void Game::update() {
-
+    m_sourceRectangle.x = 128 * int((SDL_GetTicks() / 100) % 6);
 }
 
 void Game::render() {
